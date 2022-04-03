d97785da4ebe9309b2dcbe2189af0408b98e9ff6
Carlos Alberto De Carvalho Paes <cacarvalho.paes@gmail.com>
2022-04-03 16:03:27 +0000

    Add a function playerInput to  receive input from player and check if the space is already filled

--- run.py
+++ run.py
@@ -12,6 +12,15 @@ def printBoard(board):
    print(board[6] + " | " + board[7] + " | " + board[8])
 
 # take player input
+def playerInput(board):
+    inp = int(input("Enter a number 1-9: "))
+    if inp >= 1 and inp <= 9 and board[inp-1]  == "-":
+        board[inp-1] = currentPlayer
+    else: 
+        print("Erro!")
 # check for win or tie
 # swith the player 
 # check for win or tie again 
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
